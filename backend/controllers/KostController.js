c8d8032f9092a11fbe73321b119d93520d83fdd1
Zery Aldi <zeri.aldi21@gmail.com>
2023-12-05 09:15:04 +0700

    CRUD kost

--- backend/controllers/KostController.js
+++ backend/controllers/KostController.js
@@ -1,10 +1,155 @@
-import Kost from "../models/kostModel.js";
+import { Op } from 'sequelize';
+import Kost from '../models/kostModel.js';
+import User from '../models/UserModel.js';
 
-export const getKost = async(req,res)=>{
+export const getKost = async (req, res) => {
+   try {
+      let response = await Kost.findAll();
+      res.status(200).json(response);
+   } catch (error) {
+      res.status(500).json({ msg: error.message });
+   }
+};
+export const detailKost = async (req, res) => {
     try {
-        const response = await Kost.findAll();
-        res.status(200).json(response);
+        const kost = await Kost.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+
+        if(!kost) return res.status(404).json({msg : "Data Kost Tidak Ditemukan"});
+       let response = await Kost.findOne({
+          attributes:['name', 'price','type_kost', 'land_size', 'address', 'facility', 'nearest_place', 'description'],
+          where: {
+            uuid: req.params.id
+         },
+          include: [
+             {
+                model: User,
+                attributes: ['name', 'email'],
+             },
+          ],
+       });
+       res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({msg:error.message});
+       res.status(500).json({ msg: error.message });
     }
-}
+ };
+export const kostMe = async (req, res) => {
+    try {
+       let response = await Kost.findAll({
+          attributes:['uuid','name', 'price'],
+          where: {
+            userId: req.UserId 
+         },
+          include: [
+             {
+                model: User,
+                attributes: ['name', 'email'],
+             },
+          ],
+       });
+       res.status(200).json(response);
+    } catch (error) {
+       res.status(500).json({ msg: error.message });
+    }
+ };
+export const kostById = async (req, res) => {
+    try {
+        const kost = await Kost.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+
+        if(!kost) return res.status(404).json({msg : "Data Kost Tidak Ditemukan"});
+       let response = await Kost.findOne({
+          attributes:['name', 'price'],
+          where: {
+            [Op.and]: [{ id: kost.id }, { userId: req.UserId }]
+         },
+          include: [
+             {
+                model: User,
+                attributes: ['name', 'email'],
+             },
+          ],
+       });
+       res.status(200).json(response);
+    } catch (error) {
+       res.status(500).json({ msg: error.message });
+    }
+ };
+export const createKost = async (req, res) => {
+   const { name, price, type_kost, land_size, address, facility, nearest_place, description } = req.body;
+   try {
+      await Kost.create({
+         name: name,
+         price: price,
+         type_kost: type_kost,
+         land_size: land_size,
+         address: address,
+         facility: facility,
+         nearest_place: nearest_place,
+         description: description,
+         userId: req.UserId,
+      });
+      res.status(201).json({ msg: 'Kost Berhasil Dibuat' });
+   } catch (error) {
+      res.status(400).json({ msg: error.message });
+   }
+};
+export const updateKost = async (req, res) => {
+   try {
+      const kost = await Kost.findOne({
+         where: {
+            uuid: req.params.id,
+         },
+      });
+      if (!kost) return res.status(404).json({ msg: 'Kost Tidak Ditemukan' });
+      const { name, price, type_kost, land_size, address, facility, nearest_place, description } = req.body;
+      if (req.UserId !== kost.userId) return res.status(403).json({ msg: 'Anda Tidak Dapat mengedit bukan kost anda' });
+        await Kost.update({
+            name: name,
+            price: price,
+            type_kost: type_kost,
+            land_size: land_size,
+            address: address,
+            facility: facility,
+            nearest_place: nearest_place,
+            description: description,
+         },
+         {
+            where: {
+               [Op.and]: [{ id: kost.id }, { userId: req.UserId }],
+            },
+        });
+        res.status(200).json({ msg: "kost Berhasil Di ubah"});
+   } catch (error) {
+      res.status(500).json({ msg: error.message });
+   }
+};
+export const deleteKost = async (req, res) => {
+    try {
+       const kost = await Kost.findOne({
+          where: {
+             uuid: req.params.id,
+          },
+       });
+       if (!kost) return res.status(404).json({ msg: 'Kost Tidak Ditemukan' });
+       if (req.UserId !== kost.userId) return res.status(403).json({ msg: 'Anda Tidak Dapat mengedit bukan kost anda' });
+         await Kost.destroy(
+          {
+             where: {
+                [Op.and]: [{ id: kost.id }, { userId: req.UserId }],
+             },
+         });
+         res.status(200).json({ msg: "kost Berhasil Di Hapus"});
+    } catch (error) {
+       res.status(500).json({ msg: error.message });
+    }
+ };
+
+
+
--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -22,7 +22,23 @@ export const LoginUser = createAsyncThunk("user/loginUser", async(user,thunkAPI)
             return thunkAPI.rejectWithValue(message);
         }
     }
-})
+});
+
+export const getMe = createAsyncThunk("user/getMe", async(_,thunkAPI) =>{
+    try {
+        const response = await axios.get('http://localhost:8000/me');
+        return response.data;
+    } catch (error) {
+        if (error.response){
+            const message = error.response.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
+export const LogOut = createAsyncThunk("user/LogOut", async() =>{
+    await axios.delete('http://localhost:8000/logout');
+});
 
 export const authSlice = createSlice({
     name: "auth",
@@ -40,6 +56,21 @@ export const authSlice = createSlice({
             state.user = action.payload;
         });
         builder.addCase(LoginUser.rejected, (state,action)=>{
+            state.isloading = false;
+            state.isError = true;
+            state.message = action.payload;
+        });
+
+        // GET ME
+        builder.addCase(getMe.pending, (state)=>{
+            state.isloading = true;
+        });
+        builder.addCase(getMe.fulfilled, (state, action)=>{
+            state.isloading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        });
+        builder.addCase(getMe.rejected, (state,action)=>{
             state.isloading = false;
             state.isError = true;
             state.message = action.payload;
