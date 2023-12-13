8570c536fc3931d3484e8367b5ad6286a561fbc5
Muhammad Rafiq <ibab7810@gmail.com>
2023-12-13 19:41:16 +0700

    update data kost

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { login, register, logout } = require('../controllers/authController');
+
 const { authenticateToken } = require('../middleware/authUser');
+const { getAllKost } = require('../controllers/KostController');
 const routes = express();
 
 routes.post('/register', register);
 routes.post('/login', login);
 routes.post('/logout', logout);
+routes.get('/kost', getAllKost);
 // routes.get('/secure-route', authenticateToken);
 module.exports = routes;
