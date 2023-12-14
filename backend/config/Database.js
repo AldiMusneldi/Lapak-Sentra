f36e30d4c680933a05954f0063093d1d8a2b6720
Zery Aldi <zeri.aldi21@gmail.com>
2023-12-14 22:01:13 +0700

    database .env

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise')
+const dotenv = require("dotenv");
+dotenv.config();
 
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "lapaksentra",
-  password: "",
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const { login, register, logout } = require('../controllers/authController');
-<<<<<<< HEAD
 const { authenticateToken, verifyUser,  } = require('../middleware/authUser');
 const { me } = require('../controllers/userController');
-=======
-
 const { authenticateToken } = require('../middleware/authUser');
 const { getAllKost } = require('../controllers/KostController');
->>>>>>> 8570c536fc3931d3484e8367b5ad6286a561fbc5
 const routes = express();
 
 routes.post('/register', register);
 routes.post('/login', login);
-<<<<<<< HEAD
 routes.get('/logout', logout);
 routes.get('/me',verifyUser, me);
-=======
-routes.post('/logout', logout);
 routes.get('/kost', getAllKost);
->>>>>>> 8570c536fc3931d3484e8367b5ad6286a561fbc5
 // routes.get('/secure-route', authenticateToken);
 module.exports = routes;
